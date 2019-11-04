1f09f542a55717e6686fa489c21931ae57276d4a
IdeFFiX <ideffix1337@gmail.com>
2019-11-04 23:41:41 +0100

    hiddin information into img

--- src/components/EncryptionMode/EncryptionMode.js
+++ src/components/EncryptionMode/EncryptionMode.js
@@ -1,63 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import hack from "../../images/hack.png";
-import { decryption, encryption } from "../../services/crypto-service";
+import { encryption, hideIntoImg } from "../../services/crypto-service";
+import { toBase64 } from "../../services/coding-service";
 
-const EncryptionMode = ({ imageRef }) => (
-    <Container>
-        <Form.Row>
-            <Form.Group as={Col}>
-                <Form.Label>Text</Form.Label>
-                <Form.Control as="textarea" />
-            </Form.Group>
-        </Form.Row>
-        <Form.Row>
-            <Form.Group as={Col}>
-                <Form.Label>Encryption key</Form.Label>
-                <Form.Control type="text" placeholder="Enter encryption key" />
-            </Form.Group>
-            <Form.Group as={Col}>
-                <Form.Label>Steganography key</Form.Label>
-                <Form.Control
-                    type="text"
-                    placeholder="Enter steganography key"
-                />
-            </Form.Group>
-        </Form.Row>
-        <Row>
-            <Col xs={5}>
-                <img
-                    src={imageRef.path}
-                    className={"w-100"}
-                    alt={"before-img"}
-                />
-            </Col>
-            <Col
-                xs={2}
-                className={"d-flex align-items-center cursor-on-hover"}
-                onClick={() =>
-                    decryption(encryption("some text", "s0m3k3y"), "s0m3k3y")
-                }
-            >
-                <img src={hack} className={"w-100"} alt={"hack"} />
-            </Col>
-            <Col xs={5}></Col>
-        </Row>
-        <Row>
-            <Col xs={10}>
-                <Form.Group>
-                    <Form.Label>Save path</Form.Label>
-                    <Form.Control type="text" placeholder="Enter save path" />
+const EncryptionMode = ({ imageRef }) => {
+    const [afterImage, setAfterImage] = useState(null);
+    const [form, setValues] = useState({});
+    const updateField = e => {
+        setValues({
+            ...form,
+            [e.target.name]: e.target.value
+        });
+    };
+    return (
+        <Container>
+            <Form.Row>
+                <Form.Group as={Col}>
+                    <Form.Label>Text</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        onChange={updateField}
+                        name={"text"}
+                    />
                 </Form.Group>
-            </Col>
-            <Col
-                xs={2}
-                className={"d-flex align-items-center cursor-on-hover w-100"}
-            >
-                <Button>Save</Button>
-            </Col>
-        </Row>
-    </Container>
-);
+            </Form.Row>
+            <Form.Row>
+                <Form.Group as={Col}>
+                    <Form.Label>Encryption key</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter encryption key"
+                        onChange={updateField}
+                        name={"encryptionKey"}
+                    />
+                </Form.Group>
+                <Form.Group as={Col}>
+                    <Form.Label>Steganography key</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter steganography key"
+                        onChange={updateField}
+                        name={"steganographyKey"}
+                    />
+                </Form.Group>
+            </Form.Row>
+            <Row>
+                <Col xs={5}>
+                    <img
+                        src={imageRef.path}
+                        className={"w-100"}
+                        alt={"before-img"}
+                    />
+                </Col>
+                <Col
+                    xs={2}
+                    className={"d-flex align-items-center cursor-on-hover"}
+                    onClick={() =>
+                        hideIntoImg(
+                            imageRef,
+                            encryption(form.text, form.encryptionKey),
+                            setAfterImage
+                        )
+                    }
+                >
+                    <img src={hack} className={"w-100"} alt={"hack"} />
+                </Col>
+                <Col xs={5}>
+                    {afterImage && (
+                        <img
+                            src={
+                                "data:image/bmp;base64," + toBase64(afterImage)
+                            }
+                            className={"w-100"}
+                            alt={"after-img"}
+                        />
+                    )}
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={10}>
+                    <Form.Group>
+                        <Form.Label>Save path</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Enter save path"
+                        />
+                    </Form.Group>
+                </Col>
+                <Col
+                    xs={2}
+                    className={
+                        "d-flex align-items-center cursor-on-hover w-100"
+                    }
+                >
+                    <Button>Save</Button>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
 
 export default EncryptionMode;
--- src/services/crypto-service.js
+++ src/services/crypto-service.js
@@ -1,5 +1,9 @@
 import { sha512_256 } from "js-sha512";
 import aesjs from "aes-js";
+import { readFile } from "./file-service";
+
+const LENGTH_OF_LENGTH_BYTES = 4;
+const START_POINT = 200;
 
 export const encryption = (text, key) => {
     const keyArray = sha512_256.array(key);
@@ -13,4 +17,76 @@ export const decryption = (byteArr, key) => {
     const aesCtr = new aesjs.ModeOfOperation.ctr(keyArray);
     const decryptedBytes = aesCtr.decrypt(byteArr);
     return aesjs.utils.utf8.fromBytes(decryptedBytes);
+};
+
+export const hideIntoImg = (imageRef, byteArr, onDataHide) => {
+    readFile(
+        imageRef.path,
+        imgData => {
+            const offset = writeData(
+                imgData,
+                START_POINT,
+                getInt64Bytes(byteArr.length).slice(LENGTH_OF_LENGTH_BYTES)
+            );
+            writeData(imgData, offset, byteArr);
+            onDataHide(imgData);
+        },
+        console.error
+    );
+};
+
+const writeData = (imgData, offset, data) => {
+    for (let i = 0; i < data.length; i++) {
+        const bits = decToBin(data[i]);
+        for (let j = 0; j < bits.length; j += 2) {
+            write2Bits(imgData, offset, Number(bits[j]), Number(bits[j + 1]));
+            offset += 3;
+        }
+    }
+    return offset;
+};
+
+const write2Bits = (imgData, offset, x1, x2) => {
+    const a1 = imgData[offset] % 2;
+    const a2 = imgData[offset + 1] % 2;
+    const a3 = imgData[offset + 2] % 2;
+    if (xor(a1, a3) !== x1) {
+        if (xor(a2, a3) === x2) {
+            imgData[offset] = imgData[offset] + resolveChange(a1);
+        } else {
+            imgData[offset + 2] = imgData[offset + 2] + resolveChange(a3);
+        }
+    } else if (xor(a2, a3) !== x2) {
+        imgData[offset + 1] = imgData[offset + 1] + resolveChange(a2);
+    }
+};
+
+const resolveChange = a => (a === 0 ? 1 : -1);
+
+const xor = (a, b) => (a === b ? 0 : 1);
+
+const intFromBytes = x => {
+    let val = 0;
+    for (let i = 0; i < x.length; ++i) {
+        val += x[i];
+        if (i < x.length - 1) {
+            val = val << 8;
+        }
+    }
+    return val;
+};
+
+const getInt64Bytes = x => {
+    let bytes = [];
+    let i = 8;
+    do {
+        bytes[--i] = x & 255;
+        x = x >> 8;
+    } while (i);
+    return bytes;
+};
+
+const decToBin = dec => {
+    const bin = dec.toString(2);
+    return bin.padStart(8, "0");
 };
